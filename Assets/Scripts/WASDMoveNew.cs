504411d7a572a24d7b745cfec654c0c168e88afd
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2026-01-07 21:47:46 +0200

    Daily progress

---
+++ Assets/Scripts/WASDMoveNew.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WASDMoveNew : MonoBehaviour
+{
+    PlayerInput input; // The generated C# class
+    Vector2 move;
+
+    void Awake()
+    {
+        input = new PlayerInput(); // Initialize the input
+    }
+
+    void OnEnable()
+    {
+        input.Player.Enable(); // Enable the action map
+
+        input.Player.@Move.performed += ctx => {
+            move = ctx.ReadValue<Vector2>();
+            Debug.Log("Move input: " + move); // debug log
+        };
+
+        input.Player.@Move.canceled += ctx => move = Vector2.zero;
+    }
+
+    void OnDisable()
+    {
+        input.Player.Disable();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.wKey.isPressed) Debug.Log("W pressed");
+        if (Keyboard.current.aKey.isPressed) Debug.Log("A pressed");
+        if (Keyboard.current.sKey.isPressed) Debug.Log("S pressed");
+        if (Keyboard.current.dKey.isPressed) Debug.Log("D pressed");
+        if (move != Vector2.zero)
+            Debug.Log("Move vector: " + move);
+
+        transform.Translate(new Vector3(move.x, 0, move.y) * 5f * Time.deltaTime);
+    }
+}
