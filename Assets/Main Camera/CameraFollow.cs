0ad845f88d873b536aa1dfdb17dc5a1c705f97c7
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2026-01-24 04:25:59 +0200

    Added camera follow

---
+++ Assets/Main Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 12f, -100f);
+    public float followSpeed = 1f;
+    Vector3 velocity;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(
+        transform.position,
+        desiredPos,
+        ref velocity,
+        0.15f);
+
+        transform.LookAt(target.position);
+    }
+}
