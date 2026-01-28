38a589f7397c47028c271c167dccb6d12b020ec8
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2026-01-28 23:52:15 +0200

    tried to fix bug with some skills not hitting enemies

--- Assets/Skills/Active/Laser/Laser.cs
+++ Assets/Skills/Active/Laser/Laser.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 
-public class Laser : MonoBehaviour
+public class Laser : HitSkillBase
 {
     public GameObject laserPrefab;
     public Transform firePoint;
     public float laserDuration = 0.5f;
     public float laserLength = 100f;
+    public float targetRange = 25f;
+
+    [Header("Targeting")]
+    public LayerMask enemyLayer;
 
     void Update()
     {
@@ -19,20 +23,44 @@ public class Laser : MonoBehaviour
 
     void FireLaser()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (!Physics.Raycast(ray, out RaycastHit hit))
-            return;
+        Collider[] enemies = Physics.OverlapSphere(
+            firePoint.position,
+            targetRange,
+            enemyLayer
+        );
 
-        Vector3 dir = hit.point - firePoint.position;
+        if (enemies.Length == 0)
+        {
+            Debug.Log("No enemies in range");
+            return;
+        }
+
+        // pick random enemy
+        Collider target = enemies[Random.Range(0, enemies.Length)];
+        Debug.Log("Targeting: " + target.name);
+
+        Vector3 dir = target.bounds.center - firePoint.position;
         dir.y = 0f;
         dir.Normalize();
+
+        // damage
+        Ray ray = new Ray(firePoint.position, dir);
+        if (Physics.Raycast(ray, out RaycastHit hit, laserLength, enemyLayer))
+        {
+            Debug.Log("Laser hit: " + hit.collider.name);
+            TryHit(hit.collider);
+        }
 
         SpawnLaser(firePoint.position, dir);
 
         SpellEcho echo = GetComponent<SpellEcho>();
         if (echo != null)
         {
-            StartCoroutine(EchoLaser(firePoint.position, dir, echo.echoDelay));
+            StartCoroutine(EchoLaser(
+                firePoint.position,
+                dir,
+                echo.echoDelay
+            ));
         }
     }
 
@@ -74,17 +102,5 @@ public class Laser : MonoBehaviour
             laser.transform.localScale.y * thicc.thicknessMultiplier,
             laser.transform.localScale.z
         );
-    }
-}
-
-public class LaserHit : MonoBehaviour
-{
-    void OnTriggerEnter(Collider other)
-    {
-        Enemy enemy = other.GetComponent<Enemy>();
-        if (enemy != null)
-        {
-            //enemy.TakeHit(gameObject);
-        }
     }
 }
