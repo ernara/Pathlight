9528a9fbe4c60497acda7ae97c76341b8ff3991d
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2026-02-06 00:17:16 +0200

    added clone dummy object spawning instead of player

--- Assets/Skills/Support/Clone/ClonePlayer.cs
+++ Assets/Skills/Support/Clone/ClonePlayer.cs
@@ -2,49 +2,36 @@ using UnityEngine;
 
 public class ClonePlayer : MonoBehaviour
 {
-    public GameObject clonePrefab; 
-    public int cloneCount = 10;    
+    public GameObject clonePrefab;
+    public int cloneCount = 10;
     public float spawnRadius = 5f;
-    public float cloneDuration = 15f;
 
     void Update()
     {
         if (UnityEngine.InputSystem.Keyboard.current.rKey.wasPressedThisFrame)
         {
-            Debug.Log("Manual ActivateClone trigger");
-            ActivateClone();
+            Debug.Log("SPAWNING TEST CLONES");
+            SpawnClones();
         }
     }
 
-    public void ActivateClone()
+    void SpawnClones()
     {
         if (clonePrefab == null)
         {
-            Debug.LogError("clonePrefab is null!");
+            Debug.LogError("CLONE PREFAB IS NULL!");
             return;
         }
 
         for (int i = 0; i < cloneCount; i++)
         {
             Vector2 offset = Random.insideUnitCircle * spawnRadius;
-            Vector3 spawnPos = transform.position + new Vector3(offset.x, 0, offset.y);
 
-            GameObject clone = Instantiate(clonePrefab, spawnPos, transform.rotation);
-            clone.SetActive(true);
+            Vector3 pos = transform.position + new Vector3(offset.x, 0, offset.y);
 
-            var c = clone.GetComponent<ClonePlayer>();
-            if (c != null) Destroy(c);
-
-            var mimic = clone.AddComponent<CloneMimic>();
-            mimic.Initialize(this.gameObject);
-
-            clone.layer = gameObject.layer;
-            foreach (Transform t in clone.GetComponentsInChildren<Transform>())
-                t.gameObject.layer = gameObject.layer;
-
-            Destroy(clone, cloneDuration);
+            Instantiate(clonePrefab, pos, Quaternion.identity);
         }
 
-        Debug.Log(cloneCount + " clones spawned around player!");
+        Debug.Log("CLONES SPAWNED SUCCESSFULLY");
     }
 }
