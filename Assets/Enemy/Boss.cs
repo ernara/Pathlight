3dc83875c7ae14845eeda9da046823870f29a0c8
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2026-02-13 16:51:38 +0200

    finally learned and added a way to detect spells for enemies and destroy them

--- Assets/Enemy/Boss.cs
+++ Assets/Enemy/Boss.cs
@@ -1,16 +1,39 @@
+//using UnityEngine;
+
+//public class Boss : MonoBehaviour
+//{
+//    int hits;
+
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Cube"))
+//        {
+//            other.tag = "Untagged";      
+//            hits++;
+//            if (hits >= 4) Destroy(gameObject);
+//        }
+//    }
+//}
+
+
 using UnityEngine;
+
 
 public class Boss : MonoBehaviour
 {
     int hits;
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision col)
     {
-        if (other.CompareTag("Cube"))
-        {
-            other.tag = "Untagged";      
-            hits++;
-            if (hits >= 4) Destroy(gameObject);
-        }
+        Spell spell = col.gameObject.GetComponent<Spell>();
+        if (spell == null) return;
+
+        hits++;
+        //Destroy(col.gameObject);   //sudestroyins spello
+
+        if (hits >= 4)
+            Destroy(gameObject);
     }
 }
+
+
